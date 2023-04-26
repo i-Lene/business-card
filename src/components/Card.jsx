f76dc94c58954ac4a0e9d4f40a2bce955656b9f5
Marlene <marlene.s.veiga@gmail.com>
2023-04-26 13:09:02 +0100

    deploy changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,42 +5,45 @@ import { Button } from 'react-bootstrap';
 import { Paragraph } from "./Paragraphs";
 import { Footer } from "./Footer";
 
-
-
 function Card() {
-
-    function handleButtonGitClick() {
+    const handleButtonGitClick = () => {
         window.open('https://github.com/i-Lene', '_blank');
-    }
-    function handleButtonLinkedinCLick() {
+    };
+
+    const handleButtonLinkedinCLick = () => {
         window.open('https://www.linkedin.com/in/msebastiaoveiga/', '_blank');
-    }
+    };
 
     return (
         <div className="main-card">
             <CardImage />
-            <IntroSection name="Marlene Sebastião Veiga" role="Web developer" website="https://github.com/i-Lene" websiteText="github.com/i-Lene" />
+            <IntroSection
+                name="Marlene Sebastião Veiga"
+                role="Web developer"
+                website="https://github.com/i-Lene"
+                websiteText="github.com/i-Lene"
+            />
             <Button
                 title="Github"
                 onClick={handleButtonGitClick}
                 className="button-socials git"
             >
-                <i class="fa-brands fa-github"></i>
+                <i className="fa-brands fa-github"></i>
                 Github
             </Button>
             <Button
-                title="Github"
+                title="LinkedIn"
                 onClick={handleButtonLinkedinCLick}
                 className="button-socials linkedin"
-
             >
-                <i class="fa-brands fa-linkedin"></i>
+                <i className="fa-brands fa-linkedin"></i>
                 Linkedin
             </Button>
             <Paragraph />
-            <Footer></Footer>
+            <Footer />
         </div>
-    )
+    );
 }
 
 export { Card };
+
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
 function Footer() {
     return (
         <footer className="footer">
-            <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands socials fa-square-facebook"></i></a>
-            <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
-            <a href="https://twitter.com/?lang=en" target="_blank"><i class="fa-brands fa-square-twitter"></i></a>
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                <i className="fa-brands socials fa-square-facebook"></i>
+            </a>
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                <i className="fa-brands fa-square-instagram"></i>
+            </a>
+            <a href="https://twitter.com/?lang=en" target="_blank" rel="noopener noreferrer">
+                <i className="fa-brands fa-square-twitter"></i>
+            </a>
         </footer>
-    )
+    );
 }
 
 export { Footer };
